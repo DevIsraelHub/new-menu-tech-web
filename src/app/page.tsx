08ae863a8cef3b90f73354a4b3865d4d20714ca7
Emmanuelodey2 <116392410+Emmanuelodey2@users.noreply.github.com>
2024-07-25 15:40:12 +0100

    refactor: Added a new component the cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ import Services from "@/components/Services";
 import MenuMap from "@/components/MenuMap";
 import Features from "@/components/Features";
 import About from "@/components/About";
+import CursorFollower from "./cursor";
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden ">
+      <CursorFollower />
       <Header />
       <Hero />
       <Services />
